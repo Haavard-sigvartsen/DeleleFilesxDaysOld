a994f4e6a57b22e2c4bb78171255a01df7519c04
Haavard <6866104+Haavard-sigvartsen@users.noreply.github.com>
2022-06-07 22:23:48 +0200

    Add files via upload

---
+++ DeleleFilesxDaysOld.ps1
@@ -0,0 +1,6 @@
+﻿<#  Script for removing log files older than x Days
+    Made by: Håvard Sigvartsen (Company) 07.06.2022
+#>
+$Age = (get-date).AddDays(-90) # x amount of days
+$logpath = "C:\Filer\Log\UBWSync" #path, example: C:\Filer\Log\UBWSync
+Get-ChildItem -path $logpath -Recurse -Filter "*.txt" | where-object LastWriteTime -lt $age | Remove-Item
